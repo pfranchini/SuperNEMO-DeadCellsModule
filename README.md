3d5f71c9d3ee01446422539584f07a701a66a06c
Paolo Franchini <pfranchini@gmail.com>
2020-02-26 15:57:54 +0000

    First working version. Removes hits on random dead cells.

--- README.md
+++ README.md
@@ -20,5 +20,5 @@ flreconstruct -i <brio_file_with_calibration_bank.brio> -p testmodule.conf.in
 
 ## Run the remaining reconstruction:
 ```
-flreconstruct -i <test-output.brio> -p official-2.0.0.conf -o <test-output-reco.brio>
+flreconstruct -i <test-output.brio> -p official-2.0.0_from_CAT.conf -o <test-output-reco.brio>
 ```
--- testmodule.cpp
+++ testmodule.cpp
@@ -11,15 +11,21 @@ dpp::base_module::process_status status;
 //snemo::datamodel::calibrated_data* ptr_calib_data = 0;
 snemo::datamodel::calibrated_data *new_calibrated_data;
 
+// metadata
 FLSimulate::FLSimulateArgs flSimParameters;
+
+// various variables
+std::string filename_output = "test-output.brio";
+const int N_dead_cells=100;
+int dead_cells[N_dead_cells][3];  // Define a matrix (side, layer, column) of dead cells 
+Long64_t hits = 0;
+Long64_t killed = 0;
+
 
 DPP_MODULE_REGISTRATION_IMPLEMENT(testmodule,"testmodule");
 
 
-testmodule::testmodule() : dpp::base_module()
-{
-  //filename_output_="SimValidation.root";
-}
+testmodule::testmodule() : dpp::base_module() {}
 
 
 testmodule::~testmodule() {
@@ -38,52 +44,35 @@ falaise::exit_code do_metadata( const FLSimulate::FLSimulateArgs& flSimParameter
 
   system_props.store_string("bayeux.version", bayeux::version::get_version(),
                             "Bayeux version");
-
   system_props.store_string("falaise.version", falaise::version::get_version(),
                             "Falaise version");
-
   system_props.store_string(
 			    "application", "flsimulate",
 			    "The simulation application used to produce Monte Carlo data");
-
   system_props.store_string("application.version",
                             falaise::version::get_version(),
                             "The version of the simulation application");
-
   system_props.store_string("userProfile", flSimParameters.userProfile,
                             "User profile");
-
   system_props.store_integer("numberOfEvents", flSimParameters.numberOfEvents,
                              "Number of simulated events");
-
   system_props.store_boolean("doSimulation", flSimParameters.doSimulation,
                              "Activate simulation");
-
   system_props.store_boolean("doDigitization", flSimParameters.doDigitization,
                              "Activate digitization");
-
-  if (!flSimParameters.experimentalSetupUrn.empty()) {
+  if (!flSimParameters.experimentalSetupUrn.empty())
     system_props.store_string("experimentalSetupUrn",
                               flSimParameters.experimentalSetupUrn,
                               "Experimental setup URN");
-  }
-
   system_props.store_boolean("embeddedMetadata",
                              flSimParameters.embeddedMetadata,
                              "Metadata embedding flag");
-
-  boost::posix_time::ptime start_run_timestamp =
-    boost::posix_time::second_clock::universal_time();
-  system_props.store_string(
-			    "timestamp", boost::posix_time::to_iso_string(start_run_timestamp),
-			    "Run start timestamp");
-
+  boost::posix_time::ptime start_run_timestamp = boost::posix_time::second_clock::universal_time();
+  system_props.store_string( "timestamp", boost::posix_time::to_iso_string(start_run_timestamp), "Run start timestamp");
   if (flSimParameters.doSimulation) {
     // Simulation section:
-    datatools::properties& simulation_props = flSimMetadata.add_section(
-									"flsimulate.simulation", "flsimulate::section");
+    datatools::properties& simulation_props = flSimMetadata.add_section("flsimulate.simulation", "flsimulate::section");
     simulation_props.set_description("Simulation setup parameters");
-
     if (!flSimParameters.simulationSetupUrn.empty()) {
       simulation_props.store_string("simulationSetupUrn",
                                     flSimParameters.simulationSetupUrn,
@@ -161,24 +150,21 @@ void testmodule::initialize(const datatools::properties& myConfig,
   // the key exists. datatools::properties throws an exception if
   // the key isn't in the config, so catch this if thrown and don't do
   // anything
-  //try {
-  //  myConfig.fetch("filename_out",this->filename_output_);
-  //} catch (std::logic_error& e) {
-  
-  std::cout << "============= Initialize" << std::endl;
+  try {
+    myConfig.fetch("filename_out", filename_output);
+  } catch (std::logic_error& e) {}
 
+  std::cout << "Dead cells module initialized..." << std::endl;
+  std::cout << "Output file: " << filename_output << std::endl;
+  
+ 
   flSimParameters = FLSimulate::FLSimulateArgs::makeDefault();
 
-  datatools::service_manager services("DeadCells",
-				      "SuperNEMO Dead Cells");
+  datatools::service_manager services("DeadCells", "SuperNEMO Dead Cells");
   datatools::properties services_config;
   services_config.store("name", "DeadCells");
-  services_config.store(
-			"description",
-			"SuperNEMO Demonstrator Dead Cells module");
-  std::vector<std::string> scf = {
-        "@falaise:snemo/demonstrator/geometry/"
-        "GeometryService.conf"};
+  services_config.store("description","SuperNEMO Demonstrator Dead Cells module");
+  std::vector<std::string> scf = {"@falaise:snemo/demonstrator/geometry/" "GeometryService.conf"};
   services_config.store("services.configuration_files", scf);
   services.initialize(services_config);
 
@@ -188,29 +174,49 @@ void testmodule::initialize(const datatools::properties& myConfig,
 
   // Metadata management:
   simOutput.set_name("DeadCellsOutput");
-  simOutput.set_single_output_file("test-output.brio");
+  simOutput.set_single_output_file(filename_output);
   datatools::multi_properties& metadataStore =  simOutput.grab_metadata_store();
   metadataStore = flSimMetadata;
   simOutput.initialize_simple();   
 
-
   // set up CD data bank
   new_calibrated_data = &(workItem.add<snemo::datamodel::calibrated_data>("CD"));
-  //new_calibrated_data = *ptr_calib_data;
 
-  // Define the dead cells
-  // ....
-
-
+  // Define the dead cells:
+  // Random dead cells
+  bool isNew = false;
+  std::cout << "Generating " << N_dead_cells << " random dead cells..." << std::endl;
+  for (Long64_t i=0; i<N_dead_cells; i++){
+    // need to check if the dead_cell is not already in the list                                                                                                          
+    isNew = false;
+    while (!isNew) {  // try a random cell                                                                                                                                
+      dead_cells[i][0]=rand() % 2;    // side                                                                                                                             
+      dead_cells[i][1]=rand() % 9;    // layer                                                                                                                            
+      dead_cells[i][2]=rand() % 113;  // column                                                                                                                           
+      isNew = true; // assume is new                                                                                                                                      
+      for (Long64_t j=0; j<i; j++){  // look if is alredy in the  matrix                                                                                                  
+	if ( (dead_cells[i][0]==dead_cells[j][0]) && (dead_cells[i][1]==dead_cells[j][1]) && (dead_cells[i][2]==dead_cells[j][2]) ) {
+	  isNew = false; // is already in the matrix so will try again                                                                                                    
+	  break;
+	}
+      }
+    }
+  }
+  
+  // print list of dead cells   
+  /*for (Long64_t i=0; i<N_dead_cells; i++)                                                                                                                             
+    std::cout << dead_cells[i][0] << "-" << dead_cells[i][1] << "-" << dead_cells[i][2] << std::endl;                                                                     
+  */
+  
   this->_set_initialized(true);
-
+  
 }
 
 
 //! [testmodule::Process]
-dpp::base_module::process_status
-testmodule::process(datatools::things& event) {
+dpp::base_module::process_status testmodule::process(datatools::things& event) {
   
+  bool kill=false;
   int i = 0;
 
   // Grab Calibrated Data bank
@@ -226,32 +232,38 @@ testmodule::process(datatools::things& event) {
     // Loops over tracker hits for this event
     for (const auto& trackerHitHdl : calData.calibrated_tracker_hits()) {
 
+      hits++;  // counts the number of hits for all the events
+
+      kill=false;
       // Find if this hit of this event is on any dead cell or not   
-      // .....
-
-      if ((trackerHitHdl->get_side()!=1) || (trackerHitHdl->get_layer()!=4) || (trackerHitHdl->get_row()!=86)) {
-
-	i = &trackerHitHdl - &calData.calibrated_tracker_hits()[0];
-
-	std::cout << "hit to be removed: " << i << std::endl;
-	std::cout << "Tracker Hit: " << trackerHitHdl->get_x() << "," << trackerHitHdl->get_y() << "," << trackerHitHdl->get_z() << std::endl;
-	std::cout << "             " << trackerHitHdl->get_side() << "," << trackerHitHdl->get_layer() << "," << trackerHitHdl->get_row() << std::endl;
-
-	new_hit_handle = trackerHitHdl;
-	new_calibrated_data->calibrated_tracker_hits().push_back(new_hit_handle);
-
-      }
-
-    } // for
+      for (Long64_t k=0; k<N_dead_cells; k++){
+	if ( (trackerHitHdl->get_side()==dead_cells[k][0])&&(trackerHitHdl->get_layer()==dead_cells[k][1])&&(trackerHitHdl->get_row()==dead_cells[k][2]) ){  // is dead
+	  kill=true;
+	  
+	  killed++;
+	  /*i = &trackerHitHdl - &calData.calibrated_tracker_hits()[0];
+	  std::cout << "hit to be removed: " << i << std::endl;
+	  std::cout << "Tracker Hit: " << trackerHitHdl->get_x() << "," << trackerHitHdl->get_y() << "," << trackerHitHdl->get_z() << std::endl;
+	  std::cout << "             " << trackerHitHdl->get_side() << "," << trackerHitHdl->get_layer() << "," << trackerHitHdl->get_row() << std::endl;
+	  */
+          break; // ends loop since the hit is already on one dead cell      
+	}
+	if (!kill){ // keeps the Tracker data
+	  new_hit_handle = trackerHitHdl;
+	  new_calibrated_data->calibrated_tracker_hits().push_back(new_hit_handle);
+  	}
+      } // end loop in the killing procedure  
+    } // end loop oin the tracker hits
 
     // Print output                                                                                                                                                    
-    const auto& calData2 = workItem.get<snemo::datamodel::calibrated_data>("CD");
+    /*const auto& calData2 = workItem.get<snemo::datamodel::calibrated_data>("CD");
     for (const auto& hit : calData2.calibrated_tracker_hits()) {
       std::cout << "Saved Hit: " << hit->get_x() << "," << hit->get_y() << "," << hit->get_z() << std::endl;
       std::cout << "           " << hit->get_side() << "," << hit->get_layer() << "," << hit->get_row() << std::endl;
-    }
-
-    // Calorimeter data
+      }
+    */
+    
+    // Calorimeter data kept in any case
     new_calibrated_data->calibrated_calorimeter_hits() = calData.calibrated_calorimeter_hits();
 
     // write workItem in the new brio file
@@ -279,8 +291,8 @@ testmodule::process(datatools::things& event) {
 void testmodule::reset() {
 
   // Print stats
-  //std::cout << "Total number of hits: " << hits << std::endl;
-  //std::cout << "Killed pixels: " << killed << std::endl;
+  std::cout << "Total number of hits in the tracker: " << hits << std::endl;
+  std::cout << "Killed hits: " << killed << std::endl;
 
   std::cout << "END" << std::endl;
 
